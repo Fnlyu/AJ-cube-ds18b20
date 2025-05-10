4c3d6d56c449653f7ff62f6c92aeaa7939bd42f6
FBIx0 <FBIx0024@gmail.com>
2025-05-10 18:05:31 +0800

    Enhance STM32F103C8 project configuration
    - Updated project options in `cube-ds18b20.uvoptx` to include new debug settings and target driver configurations.
    - Modified project structure in `cube-ds18b20.uvprojx` to add I2C support and adjust memory settings.
    - Added I2C peripheral configuration in `cube-ds18b20.ioc`, including SCL and SDA pin assignments.
    - Included new source files for I2C functionality in the project structure.

--- Debug/Core/Src/oled.d
+++ Debug/Core/Src/oled.d
@@ -1,5 +1,4 @@
-Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.o: \
- ../Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c \
+Core/Src/oled.o: ../Core/Src/oled.c ../Core/Inc/oled.h ../Core/Inc/main.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
  ../Core/Inc/stm32f1xx_hal_conf.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
@@ -23,7 +22,10 @@ Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.o: \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_i2c.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
- ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h \
+ ../Core/Inc/oledfont.h ../Core/Inc/i2c.h
+../Core/Inc/oled.h:
+../Core/Inc/main.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
 ../Core/Inc/stm32f1xx_hal_conf.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
@@ -48,3 +50,5 @@ Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.o: \
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_i2c.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h:
+../Core/Inc/oledfont.h:
+../Core/Inc/i2c.h:
